6e6d966a7e121022777187f3c5e415823ebac1ca
iustiB <claudiu.boiculese@gmail.com>
2016-11-23 11:25:07 +0200

    Cherry picked commit for DB helper
    befada83de2f3df6d82d0309b3ab223fe907bcb4

--- src/main/java/ro/ubbcluj/cs/mormont/database/DBHelper.java
+++ src/main/java/ro/ubbcluj/cs/mormont/database/DBHelper.java
@@ -5,6 +5,7 @@ import ro.ubbcluj.cs.mormont.Domain.User;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Iusti on 15/11/2016.
@@ -16,12 +17,37 @@ public class DBHelper {
 
     String sql = "INSERT INTO mormont.users (username, password) VALUES (?,?)";
 
-    public DBHelper(JdbcTemplate jdbcTemplate) {
 
+    public DBHelper(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
+    public static void fillUsers() {
 
-    public static ArrayList<User> findUsers(String username, String admin) {
-        throw new NotImplementedException();
+        for (int i = 1; i < 10; i++) {
+            addUser("user" + i, "pass" + i);
+        }
+    }
+
+    public static User findUser(String username) {
+
+        List<User> users = jdbcTemplate.query("SELECT id,username, password FROM mormont.users WHERE username = ?", new Object[]{username},
+                (rs, rowNumber) -> new User(rs.getInt("id"), rs.getString("password"), rs.getString("password")));
+
+        return users.size() == 0 ? null : users.get(0);
+    }
+
+    public static List<User> findUsers(String category, String atribute) {
+
+        List<User> users = jdbcTemplate.query("SELECT id, username , password FROM mormont.users WHERE ? = ?", new Object[]{category, atribute},
+                (rs, rowNumber) -> new User(rs.getInt("id"), rs.getString("password"), rs.getString("password")));
+
+        return users.size() == 0 ? null : users;
+    }
+
+    public static void addUser(String username, String password) {
+        String sql = "INSERT INTO mormont.users (username, password) VALUES (?,?)";
+        jdbcTemplate.update(sql, new Object[]{username, password});
+
     }
 }
