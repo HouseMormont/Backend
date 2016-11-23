befada83de2f3df6d82d0309b3ab223fe907bcb4
iustiB <claudiu.boiculese@gmail.com>
2016-11-23 11:25:07 +0200

    DBHelper - update( find,  fill)

--- src/main/java/ro/ubbcluj/cs/mormont/database/DBHelper.java
+++ src/main/java/ro/ubbcluj/cs/mormont/database/DBHelper.java
@@ -12,9 +12,36 @@ public class DBHelper {
 
     private static JdbcTemplate jdbcTemplate;
 
+    public DBHelper(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
+    public static void fillUsers() {
+
+        for (int i = 1; i < 10; i++) {
+            addUser("user" + i, "pass" + i);
+        }
+    }
+
+    public static User findUser(String username) {
+
+        List<User> users = jdbcTemplate.query("SELECT id,username, password FROM mormont.users WHERE username = ?", new Object[]{username},
+                (rs, rowNumber) -> new User(rs.getInt("id"), rs.getString("password"), rs.getString("password")));
+
+        return users.size() == 0 ? null : users.get(0);
+    }
+
+    public static List<User> findUsers(String category, String atribute) {
+
+        List<User> users = jdbcTemplate.query("SELECT id, username , password FROM mormont.users WHERE ? = ?", new Object[]{category, atribute},
+                (rs, rowNumber) -> new User(rs.getInt("id"), rs.getString("password"), rs.getString("password")));
+
+        return users.size() == 0 ? null : users;
+    }
+
+    public static void addUser(String username, String password) {
         String sql = "INSERT INTO mormont.users (username, password) VALUES (?,?)";
+        jdbcTemplate.update(sql, new Object[]{username, password});
 
     }
 }
