24c301f365f30f095ef4091dc018e5be3608ce00
tudorlozba <tudor.lozba@gmail.com>
2017-01-23 01:24:29 +0200

    fix

--- src/main/java/ro/ubbcluj/cs/mormont/Service.java
+++ src/main/java/ro/ubbcluj/cs/mormont/Service.java
@@ -39,17 +39,6 @@ public class Service {
     public static final String MAIL_USER = "tudor_l95@yahoo.com";
     public static final String MAIL_PASSWORD = "toshiba";
 
-    @NotNull
-    private static Properties getMailProperties() {
-        Properties props = new Properties();
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.socketFactory.port", "465");
-        props.put("mail.smtp.socketFactory.class",
-                "javax.net.ssl.SSLSocketFactory");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.port", "465");
-        return props;
-    }
 
     public void createNewDocument(String username, String document, String documentType) {
 
